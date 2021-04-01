471f698e5a5a973d2d4ce2c2f6dc2e43a3d53044
YATNISHMANIK <77111329+YATNISHMANIK@users.noreply.github.com>
2021-04-01 09:25:41 +0530

    Add files via upload

---
+++ input output conversion .py
@@ -0,0 +1,22 @@
+# type conversion
+x='20'
+y=20
+z=int(x)+(y) # here x convert into integer from str
+print(z)
+
+# 0utput statements
+print('cool person')
+
+print('cool person is',end=' ')
+print('always killer')
+
+print('my','name','is khan',sep='**')
+
+# input statements
+input('enter the str=')
+input('enter the value=')
+# type conversion in input
+x=int(input('enter the value='))
+y=str(input('enter the name='))
+
+
